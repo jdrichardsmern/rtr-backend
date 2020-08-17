405cfb2ae8ef22145e0dee762bad7414d6310783
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-08-17 01:16:24 -0400

    added priceajust.js

---
+++ routes/middleware/stockPriceAjustment.js
@@ -0,0 +1,11 @@
+const priceAjust = (price , stockUnit, units) => {
+    let ajustedPrice = price
+    for (let i = units ; i > 0 ; i--){
+      const rng = Math.floor(Math.random() * 5) /// 1 - 5%
+      ajustedPrice += (rng/stockUnit)  * price
+    }
+     return ajustedPrice  
+    }
+    
+
+    module.exports = priceAjust
